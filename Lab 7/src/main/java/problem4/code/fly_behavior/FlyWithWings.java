2d601666f4df8d18e7ab9d786cc98aa25db7ee06
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2025-01-23 06:08:43 -0600

    Init Commit Lab 7 problem4

---
+++ Lab 7/src/main/java/problem4/code/fly_behavior/FlyWithWings.java
@@ -0,0 +1,8 @@
+package problem4.code.fly_behavior;
+
+public class FlyWithWings implements FlyBehavior{
+    @Override
+    public void fly() {
+        System.out.println(" fly with wings");
+    }
+}
---
+++ Lab 7/src/main/java/problem4/code/quack_behavior/Squeak.java
@@ -0,0 +1,8 @@
+package problem4.code.quack_behavior;
+
+public class Squeak implements QuackBehavior{
+    @Override
+    public void quack() {
+        System.out.println(" squeaking");
+    }
+}
