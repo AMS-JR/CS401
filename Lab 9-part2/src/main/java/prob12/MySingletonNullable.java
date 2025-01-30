62c90f24a5274ad2b31ad9d41af8baf780a8a018
AMS-JR <asj.sarjo@gmail.com>
2025-01-30 16:03:39 -0600

    Commit Lab project 9-part2

---
+++ Lab 9-part2/src/main/java/prob12/MySingletonNullable.java
@@ -0,0 +1,27 @@
+package prob12;
+
+import java.util.Optional;
+
+public class MySingletonNullable {
+    private static int count = 0;
+    private static MySingletonNullable instance = null;
+    private MySingletonNullable() {
+        count++;
+    }
+    public static MySingletonNullable getInstance() {
+        return Optional.ofNullable(instance).orElseGet(() -> createInstance());
+        //return Optional.ofNullable(instance).orElseGet(MySingletonOfNullable::setAndGet);
+    }
+
+    private static MySingletonNullable createInstance() {
+        instance = new MySingletonNullable();
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; ++i) {
+            getInstance();
+        }
+        System.out.println(count);
+    }
+}
